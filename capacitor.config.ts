a8095b3edf79317fc6769b32d077bd77a66aceae
Jr Klaus MOMBO-NZAHOU <juniorklausd@gmail.com>
2024-07-26 07:24:56 +0000

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.capacitorioshttp.bug',
+  appName: 'capacitor-http-bug',
+  webDir: 'dist/reproduce-capacitor-http-bug',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    }
+  }
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,4 +11,30 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'reproduce-capacitor-http-bug';
+  public result = '';
+  public isLoading = false;
+
+  constructor(private http: HttpClient) {}
+
+  sendRequest() {
+    this.result = '';
+    this.isLoading = true;
+
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1', {
+      params: {
+        categoryName: 'Asset Type',
+        toDate: (new Date()).toISOString()
+      }
+    }).subscribe({
+      next: (data) => {
+        this.isLoading = false;
+        this.result = 'OK';
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.result = 'Error';
+        console.error(error);
+      }
+    });
+  }
 }
