0b4cb95e19389ef9cf0e300f82440f658d443a98
jkd2bleu <juniorklausd@gmail.com>
2024-07-26 07:51:47 +0000

    adding ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'xyz.capacitorioshttp.bug',
   appName: 'capacitor-http-bug',
-  webDir: 'dist/reproduce-capacitor-http-bug',
+  webDir: 'dist/reproduce-capacitor-http-bug/browser',
   plugins: {
     CapacitorHttp: {
       enabled: true,
